aeda4ad0c6559f24221d200793296ae2891b6185
Thaís Marchetti Contó <116967975+thaisconto@users.noreply.github.com>
2025-03-19 20:07:18 -0300

    lista

--- aula/app/src/main/java/com/example/aula/MainActivity3.java
+++ aula/app/src/main/java/com/example/aula/MainActivity3.java
@@ -1,6 +1,11 @@
 package com.example.aula;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +25,33 @@ public class MainActivity3 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        final Button button = (Button) findViewById(R.id.botaoOk);
+        button.setOnClickListener(new Button.OnClickListener() {
+
+            public void onClick(View v) {
+                EditText campoEndereco = (EditText)
+                        findViewById(R.id.campoEndereco);
+                String endereco = campoEndereco.getText().toString();
+                Uri uri = Uri.parse(endereco);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+
+        });
+
+        final Button button2 = (Button) findViewById(R.id.botao2);
+        button.setOnClickListener(new Button.OnClickListener(){
+
+            public void onClick(View v) {
+                EditText campoTelefone = (EditText) findViewById(R.id.
+                        campoTelefone);
+                String telefone = campoTelefone.getText().toString();
+                Uri uri = Uri.parse("tel:"+telefone);
+                Intent intent = new Intent(Intent.ACTION_CALL, uri);
+                startActivity(intent);
+            }
+        });
+
     }
 }
